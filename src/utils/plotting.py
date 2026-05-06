315e50a2eefa1d100fd98c95724620e0d1958c22
Marcelo Jorge <mcaschultz@hotmail.com>
2026-05-06 16:13:32 -0300

    feat: Implementa FFT.

--- src/utils/plotting.py
+++ src/utils/plotting.py
@@ -1,5 +1,9 @@
+# utils/plotting.py
+
 import matplotlib.pyplot as plt
 import pandas as pd
+import scipy
+import numpy as np
 
 def plot_oscillator_graph(metadata: list[dict], data:list[pd.DataFrame]) -> None:
     """
@@ -9,17 +13,56 @@ def plot_oscillator_graph(metadata: list[dict], data:list[pd.DataFrame]) -> None
     - metadada: Lista com os metadados dos canais.
     - data: Lista com os dados dos canais.
     """
-    
+
     if len(metadata) != len(data):
         print("Numero incompativel entre metadata e data.")
         return None
 
+    plt.figure()
     for i in range(len(data)):
-        plt.plot((data[i]["time"]) / float(metadata[i]["Horizontal Scale"]) ,(data[i]["signal"] + float(metadata[i]["Vertical Offset"])) / float(metadata[i]["Vertical Scale"]) , 'o', label = metadata[i]["Source"])
+        x = (data[i]["time"]) / float(metadata[i]["Horizontal Scale"])
+        y = (data[i]["signal"] - float(metadata[i]["Vertical Offset"])) / float(metadata[i]["Vertical Scale"])
+        plt.plot(x,y , 'o' , label = metadata[i]["Source"], markersize=5)
 
     plt.legend()
     plt.xlabel(metadata[0]["Horizontal Units"])
     plt.ylabel(metadata[0]["Vertical Units"])
-    plt.show()
+    #plt.show()
 
     return None
+
+def plot_oscillator_graph_curve(metadata: list[dict], data:list[pd.DataFrame]):
+    plt.figure()
+    def sin_function(x,amplitude,omega,phase,offset):
+        return amplitude * np.sin((x * omega) + phase) + offset
+    
+    for i in range(len(data)):
+        raw_x = (data[i]["time"]) * float(metadata[i]["Horizontal Scale"])
+        y = (data[i]["signal"] - float(metadata[i]["Vertical Offset"])) / float(metadata[i]["Vertical Scale"])            
+        
+        N = len(y)
+        
+        x_indices = np.arange(N)
+
+        guess_offset = np.mean(y)
+
+        yf = scipy.fft.rfft(y - guess_offset)
+        xf = scipy.fft.rfftfreq(N, d=1.0)
+        
+        idx_max = np.argmax(np.abs(yf))
+        guess_omega_idx = 2 * np.pi * xf[idx_max]
+        guess_amplitude = (np.max(y) - np.min(y)) / 2
+        
+        p0 = [guess_amplitude, guess_omega_idx, 0, guess_offset]
+
+        popt, pcov = scipy.optimize.curve_fit(sin_function,x_indices,y,p0=p0)
+        plt.plot(raw_x, sin_function(x_indices, *popt), label = metadata[i]["Source"])
+        print(metadata[i]["Source"])
+        print(popt)
+
+    plt.legend()
+    plt.xlabel(metadata[0]["Horizontal Units"])
+    plt.ylabel(metadata[0]["Vertical Units"])
+    #plt.show()
+    return None
+
